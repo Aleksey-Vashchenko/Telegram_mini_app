4ece96baf72ad516c4d29c13c37c79eac8993432
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-10-18 01:15:00 +0300

    back: init
    Init backend project

---
+++ Backend/src/main/java/com/telegram/app/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
